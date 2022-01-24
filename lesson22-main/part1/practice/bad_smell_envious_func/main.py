c18100c299861e424295327f2d6c75e57f2525b2
NatalyaKras <96298804+NatalyaKras@users.noreply.github.com>
2022-01-24 22:56:56 +0400

    Add files via upload

---
+++ lesson22-main/part1/practice/bad_smell_envious_func/main.py
@@ -0,0 +1,20 @@
+# Вам не кажется, что CubeVolumeCalculator 
+# чаще дергает методы класса Cube? Исправьте так, 
+# чтобы избавиться от лишних обращений к классу Cube
+
+
+class Cube:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def cube_volume(self):
+        self.x*self.y*self.z
+
+class CubeVolumeCalculator:
+
+    @staticmethod
+    def calc_cube_volume(cube):
+        return cube_volume()
